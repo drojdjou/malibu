78208d0b7748fd7ece15e872a01fdf291319935e
bartekd <bartek@everyday3d.com>
2014-12-03 15:42:39 -0800

    Just rebuilding and testing bower

--- src/Version.js
+++ src/Version.js
@@ -1,2 +1,2 @@
 /** DO NOT EDIT. Updated from version.json **/
-var Framework = {"version":"3","build":4,"date":"2014-12-02T01:51:32.035Z"}
+var Framework = {"version":"3","build":5,"date":"2014-12-03T23:42:38.867Z"}
