3573cedacfe67ec003704e160ebc57bd5ab359b1
bartekd <bartek@everyday3d.com>
2014-12-03 22:07:52 -0800

    Adding Gesture utility

--- malibu.js
+++ malibu.js
@@ -1,7 +1,7 @@
 /* --- --- [Version] --- --- */
 
 /** DO NOT EDIT. Updated from version.json **/
-var Framework = {"version":"3","build":7,"date":"2014-12-04T06:06:18.455Z"}
+var Framework = {"version":"3","build":8,"date":"2014-12-04T06:07:52.086Z"}
 
 /* --- --- [Simplrz] --- --- */
 
--- src/Version.js
+++ src/Version.js
@@ -1,2 +1,2 @@
 /** DO NOT EDIT. Updated from version.json **/
-var Framework = {"version":"3","build":7,"date":"2014-12-04T06:06:18.455Z"}
+var Framework = {"version":"3","build":8,"date":"2014-12-04T06:07:52.086Z"}
